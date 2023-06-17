7761844bc14d6a65a201e8fc666feb50a60aa868
Aerhhh <callum@aerh.net>
2023-06-17 16:04:33 +0100

    ignoreCancelled & other misc changes

--- src/main/java/net/aerh/exporter/listener/BottleThrowListener.java
+++ src/main/java/net/aerh/exporter/listener/BottleThrowListener.java
@@ -1,12 +1,12 @@
 package net.aerh.exporter.listener;
 
 import net.aerh.exporter.EXPorterPlugin;
-import org.bukkit.entity.Projectile;
 import org.bukkit.entity.ThrownExpBottle;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ExpBottleEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.persistence.PersistentDataType;
 
@@ -16,22 +16,19 @@ import static net.aerh.exporter.util.Util.XP_KEY;
 
 public class BottleThrowListener implements Listener {
 
-    @EventHandler
+    @EventHandler (ignoreCancelled = true)
     public void onBottleThrow(ProjectileLaunchEvent event) {
-        Projectile projectile = event.getEntity();
-
-        if (!(projectile instanceof ThrownExpBottle)) {
+        if (!(event.getEntity() instanceof ThrownExpBottle bottle)) {
             return;
         }
 
-        ThrownExpBottle bottle = (ThrownExpBottle) projectile;
-
-        if (bottle.getItem().getItemMeta() == null) {
+        ItemMeta itemMeta = bottle.getItem().getItemMeta();
+        if (itemMeta == null) {
             return;
         }
 
-        if (bottle.getItem().getItemMeta().getPersistentDataContainer().has(XP_KEY, PersistentDataType.INTEGER)) {
-            projectile.setMetadata(STORED_XP, new FixedMetadataValue(EXPorterPlugin.getPlugin(), bottle.getItem().getItemMeta().getPersistentDataContainer().get(XP_KEY, PersistentDataType.INTEGER)));
+        if (itemMeta.getPersistentDataContainer().has(XP_KEY, PersistentDataType.INTEGER)) {
+            bottle.setMetadata(STORED_XP, new FixedMetadataValue(EXPorterPlugin.getPlugin(), bottle.getItem().getItemMeta().getPersistentDataContainer().get(XP_KEY, PersistentDataType.INTEGER)));
         }
     }
 
@@ -40,7 +37,7 @@ public class BottleThrowListener implements Listener {
         ThrownExpBottle bottle = event.getEntity();
 
         if (bottle.hasMetadata(STORED_XP)) {
-            event.setExperience(event.getEntity().getMetadata(STORED_XP).get(0).asInt());
+            event.setExperience(bottle.getMetadata(STORED_XP).get(0).asInt());
         }
     }
 }
